b185fdd4692223d68b67ea495b6049eeb4cda82b
Fernando Santos <140116671+nanobempaz@users.noreply.github.com>
2023-09-05 00:42:30 -0300

    Add files via upload

---
+++ src/br/com/neuro/Main.java
@@ -0,0 +1,35 @@
+package br.com.neuro;
+
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		
+		System.out.println("Fatos sobre neurociência");
+		System.out.println("O cérerbo é incrivelmento complexo - Neuroplasticidade - Hemisferios cerebrais - Sinapses - "
+				+ "Neurociência e Eduação");
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite um valor numérico de 1 a 5, o Fato será dado de acordado com a opção numerica que você "
+				+ "digitar");
+		System.out.println("------------------------------------------------------------------------------------");
+		Metodos fatos = new Metodos();
+		int x = scanner.nextInt();
+		
+		if(x == 1) {
+			fatos.fato1();
+		}else if(x == 2) {
+			fatos.fato2();
+		}else if(x == 3) {
+			fatos.fato3();
+		}else if(x == 4) {
+			fatos.fato4();
+		}else if(x == 5){
+			fatos.fato5();
+		}else {
+			System.out.println("Opeção inválida");
+		}
+		scanner.close();
+	}
+	
+}
---
+++ src/br/com/neuro/Metodos.java
@@ -0,0 +1,47 @@
+package br.com.neuro;
+
+public class Metodos {
+	public static void fato1() {
+		System.out.println("O Cérebro é Incrivelmente Complexo: "
+				+ "O cérebro humano é uma das estruturas mais complexas "
+				+ "do universo conhecido. Ele contém cerca de 86 bilhões "
+				+ "de neurônios interconectados, "
+				+ "cada um com milhares de conexões sinápticas.");
+	}
+	
+	public static void fato2() {
+		System.out.println("Neuroplasticidade: "
+				+ "O cérebro tem a capacidade "
+				+ "de se reorganizar e se adaptar "
+				+ "ao longo da vida, o que é conhecido"
+				+ " como neuroplasticidade. "
+				+ "Isso significa que ele pode aprender, "
+				+ "se recuperar de lesões e se adaptar a "
+				+ "novas circunstâncias.");
+	}
+	
+	public static void fato3(){
+		System.out.println("Hemisférios Cerebrais: "
+				+ "O cérebro humano é dividido em "
+				+ "dois hemisférios: o esquerdo e o direito."
+				+ " Cada hemisfério desempenha funções "
+				+ "diferentes. Por exemplo, o "
+				+ "hemisfério esquerdo geralmente "
+				+ "está associado à linguagem e à lógica, enquanto o"
+				+ " hemisfério direito está associado à criatividade e à intuição.");
+	}
+	
+	public static void fato4(){
+		System.out.println("Sinapses: As sinapses são as conexões entre os neurônios "
+				+ "que permitem a comunicação no cérebro. Existem trilhões de sinapses "
+				+ "no cérebro humano, formando uma rede complexa que sustenta todas as"
+				+ " funções cognitivas e comportamentais.");
+	}
+	
+	public static void fato5(){
+		System.out.println("Neurociência e Educação: Estudos em neurociência têm "
+				+ "influenciado a educação ao fornecer"
+				+ " insights sobre como o cérebro aprende e como otimizar o ensino e a "
+				+ "aprendizagem.");
+	}	
+}
